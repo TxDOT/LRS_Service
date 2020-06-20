338cad4c6d9a28f38d0cd98d6d1bab0c87ddf395
mwashbur <35734485+mwashbur@users.noreply.github.com>
2020-06-20 15:35:56 -0500

    populate form, btn focus, geolocation

---
+++ lrs_api.js
@@ -0,0 +1,36 @@
+require([
+    // esri config, core, and tasks
+    "esri/config",
+    "esri/tasks/support/Query",
+    "esri/tasks/QueryTask",
+    // geometry
+    "esri/geometry/Extent",
+    "esri/geometry/geometryEngineAsync",
+    "esri/geometry/SpatialReference",
+    "esri/geometry/Point",
+    "esri/geometry/Polyline",
+    // dojo
+    "dojo/dom",
+    "dojo/on",
+    "dojo/parser",
+    "dojo/domReady!"
+],
+    function(
+        esriConfig,
+        Query,
+        QueryTask,
+        Extent,
+        geometryEngineAsync,
+        SpatialReference,
+        Point,
+        Polyline,
+        dom,
+        on,
+        parser)
+    {
+        // DEFINE ALL FUNCTIONS FIRST
+
+        //
+
+    }
+);
