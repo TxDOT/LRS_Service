a0cd6cdd31a3a5afb254db4548dc2fc7f8a3d68f
mwashbur <35734485+mwashbur@users.noreply.github.com>
2020-06-28 13:14:25 -0500

    dfo only (correct Ms)

--- scripts/lrs.js
+++ scripts/lrs.js
@@ -4,14 +4,15 @@ let lrsAPI = {
     // firstIntersectingPoint:"",
     lrs: {}
 };
-// GLOBAL VARIABLES
+// GLOBAL letIABLES
 let featurePartIndex;
-let firstIntersectingPoint;
+let gidWithMeasuresGeom;
+// let firstIntersectingPoint;
 // let lrs={};
 
 //Check if using IE and get version
 function isIE() {
-    var ua = window.navigator.userAgent;
+    let ua = window.navigator.userAgent;
     return /MSIE|Trident/.test(ua);
 }
 
@@ -54,6 +55,7 @@ require([
         // Attaching functions to global API object so they are accessible outside their scope
         lrsAPI.getParams = getParams;
         lrsAPI.identRouteForM = identRouteForM;
+        lrsAPI.getSegmentWithDFO = getSegmentWithDFO;
         lrsAPI.getPointM = getPointM;
         lrsAPI.findNearestCoordinate = findNearestCoordinate;
         lrsAPI.createTwoPointPolyline = createTwoPointPolyline;
@@ -68,8 +70,8 @@ require([
             });
             // let point = new Point({ //desktop calibration
             //     type: "point",
-            //     latitude: 29.68013,
-            //     longitude: -98.4536,
+            //     latitude: 30.50746964416424,
+            //     longitude: -97.74142815453732,
             //     spatialReference: new SpatialReference({wkid: 102100})
             // });
             return point;
@@ -108,69 +110,91 @@ require([
 
             queryTask.url = "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Roadways/FeatureServer/0";
             queryTask.execute(query).then(function(results){
-                lrsAPI.lrs.dfo = getPointM(point,results);
-                dom.byId(domNode).innerHTML += JSON.stringify(lrsAPI.lrs.dfo,null,2);
+                getSegmentWithDFO(point,results,domNode);
             });
             // queryTask.execute(query).then(function(results){
             //      lrsAPI.lrs.dfo = results.features[0].attributes;
             //      // alert(JSON.stringify(lrsAPI.lrs.dfo,null,2)); //use for mobile calibration
             // });
-            console.log(lrsAPI.lrs);
+            // console.log(lrsAPI.lrs);
             // return lrsAPI.lrs;
             // document.getElementById("outputResponse").innerHTML = "Hello There!";
         }
 
+        function getSegmentWithDFO(point,results,domNode) {
+            console.log(gidWithMeasuresGeom);
+            let xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+                    // else {
+                        gidWithMeasuresGeom = xmlhttp.response;
+                    // }
+                    lrsAPI.lrs.dfo = getPointM(point,results);
+                    dom.byId(domNode).innerHTML += JSON.stringify(lrsAPI.lrs.dfo,null,2);
+                }
+            };
+
+            let serviceString = "https://services.arcgis.com/KTcxiTD9dsQw4r7Z/arcgis/rest/services/TxDOT_Roadways/FeatureServer/0";
+            let paraString = "/query?f=json&where=GID=" + results.features[0].attributes.GID + "&returnGeometry=true&returnM=true&outSR=102100";
+            let queryString = serviceString + paraString;
+            xmlhttp.open("GET", queryString, true);
+            xmlhttp.responseType = 'json';
+            xmlhttp.send();
+        }
+
         // Callback function from queryTask in identRouteForM
         // Takes results from REST endpoint call and gets measure for point
-        function getPointM(point,gidWithM) {
+        function getPointM(point,results) {
             // let mapPoint = mapPt;
-            var calculatedM = 0;
-            let gidWithMeasuresGeom = gidWithM;
-            firstIntersectingPoint = findNearestCoordinate(point,gidWithMeasuresGeom);
-            var firstIntersectingCoord = geometryEngine.nearestVertex(gidWithMeasuresGeom.features[featurePartIndex].geometry, firstIntersectingPoint);
-            var vertexIndex = firstIntersectingCoord.vertexIndex;
-            var maxVertex = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0].length-1;
+            let attrs = results.features[0].attributes;
+            let calculatedM = 0;
+            // gidWithMeasuresGeom = gidWithM;
+            console.log(gidWithMeasuresGeom); //problem is with global let scope/namespace pollution -mw
+            let firstIntersectingPoint = findNearestCoordinate(point);
+            let firstIntersectingCoord = geometryEngine.nearestVertex(gidWithMeasuresGeom.features[featurePartIndex].geometry, firstIntersectingPoint);
+            let vertexIndex = firstIntersectingCoord.vertexIndex;
+            let maxVertex = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0].length-1;
 
             if (vertexIndex == 0) {
-                var coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
-                var thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
-                var point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
-                var geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
-                var geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
+                let coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
+                let thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
+                let point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
+                let geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
+                let geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
                 calculatedM = point2M + geoMileValue;
             }
 
             if (vertexIndex == maxVertex) {
-                var coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
-                var thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
-                var point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
-                var geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
-                var geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
+                let coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
+                let thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
+                let point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
+                let geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
+                let geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
                 calculatedM = point2M - geoMileValue;
             }
 
             if (vertexIndex > 0 && vertexIndex < maxVertex) {
-                var coord1 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex-1];
-                var coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
-                var coord3 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex+1];
+                let coord1 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex-1];
+                let coord2 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex];
+                let coord3 = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex+1];
 
-                var thePoint1 = new Point([coord1[0],coord1[1]], new SpatialReference({wkid: 3857 }));
-                var thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
-                var thePoint3 = new Point([coord3[0],coord3[1]], new SpatialReference({wkid: 3857 }));
+                let thePoint1 = new Point([coord1[0],coord1[1]], new SpatialReference({wkid: 3857 }));
+                let thePoint2 = new Point([coord2[0],coord2[1]], new SpatialReference({wkid: 3857 }));
+                let thePoint3 = new Point([coord3[0],coord3[1]], new SpatialReference({wkid: 3857 }));
 
-                var firstTestSegment = createTwoPointPolyline(thePoint1,thePoint2);
-                var secondTestSegment = createTwoPointPolyline(thePoint2,thePoint3);
+                let firstTestSegment = createTwoPointPolyline(thePoint1,thePoint2);
+                let secondTestSegment = createTwoPointPolyline(thePoint2,thePoint3);
 
-                var intersectsFirstSegment = geometryEngine.intersects(firstIntersectingPoint,firstTestSegment)
-                var intersectsSecondSegment = geometryEngine.intersects(firstIntersectingPoint,secondTestSegment)
-                var calculatedM = 0;
+                let intersectsFirstSegment = geometryEngine.intersects(firstIntersectingPoint,firstTestSegment)
+                let intersectsSecondSegment = geometryEngine.intersects(firstIntersectingPoint,secondTestSegment)
 
-                var point1M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex-1][2];
-                var point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
-                var point3M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex+1][2];
+                let point1M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex-1][2];
+                let point2M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex][2];
+                let point3M = gidWithMeasuresGeom.features[featurePartIndex].geometry.paths[0][vertexIndex+1][2];
 
-                var geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
-                var geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
+                let geoMeterLength = geometryEngine.geodesicLength(createTwoPointPolyline(thePoint2,firstIntersectingPoint), 9001);
+                let geoMileValue = ((geoMeterLength/1609.344)*1000)/1000;
 
                 if (intersectsFirstSegment) {
                     if (point1M>point2M) {
@@ -189,10 +213,12 @@ require([
                     }
                 }
             }
+            console.log(gidWithMeasuresGeom);
             let displayM = Math.round(calculatedM*10000)/10000;
             let displayX = Math.round(firstIntersectingPoint.latitude*100000)/100000;
             let displayY = Math.round(firstIntersectingPoint.longitude*100000)/100000;
-            let attr = gidWithMeasuresGeom.features[0].attributes;
+            // let attr = gidWithMeasuresGeom.features[0].attributes;
+
 
             // if(attr.ASSET_NM){
             //     attr.CALC_MPT = displayM;
@@ -203,24 +229,24 @@ require([
             //     return attr;
             // }
             // else {
-                attr.CALC_DFO = displayM;
-                attr.LAT = displayX;
-                attr.LONG = displayY;
-                console.log(attr);
+                attrs.CALC_DFO = displayM;
+                attrs.LAT = displayX;
+                attrs.LONG = displayY;
+                console.log(attrs);
                 // alert(JSON.stringify(attr,null,2));
-                return attr;
+                return attrs;
             // }
         }
 
         // Gets nearest point on route based on xy ("snaps" to route)
-        function findNearestCoordinate(point,gidWithM) {
-            let gidWithMeasuresGeom = gidWithM;
-            var shortestDistance = 20000;
-            var firstIntersectingPoint;
-            var firstIntersectingPointTemp;
+        function findNearestCoordinate(point) {
+            // let gidWithMeasuresGeom = gidWithM;
+            let shortestDistance = 20000;
+            let firstIntersectingPoint;
+            let firstIntersectingPointTemp;
             featurePartIndex = 0;
 
-            for (var h=0; h < gidWithMeasuresGeom.features.length; h++) {
+            for (let h=0; h < gidWithMeasuresGeom.features.length; h++) {
                 firstIntersectingPointTemp = geometryEngine.nearestCoordinate(gidWithMeasuresGeom.features[h].geometry, point);
                 if (firstIntersectingPointTemp.distance < shortestDistance) {
                     shortestDistance = firstIntersectingPointTemp.distance;
@@ -228,15 +254,15 @@ require([
                     featurePartIndex = h;
                 }
             }
-            var returnPoint = new Point([firstIntersectingPoint.coordinate.x,firstIntersectingPoint.coordinate.y], new SpatialReference({wkid: 3857 }));
+            let returnPoint = new Point([firstIntersectingPoint.coordinate.x,firstIntersectingPoint.coordinate.y], new SpatialReference({wkid: 3857 }));
             return returnPoint;
         }
 
         // Create line to test for ascending or descending measures
         function createTwoPointPolyline(point1, point2) {
-            var newPolyline = new Polyline(new SpatialReference({wkid:3857}));
+            let newPolyline = new Polyline(new SpatialReference({wkid:3857}));
             newPolyline.type = "polyline";
-            var tmpAttLine = [];
+            let tmpAttLine = [];
             tmpAttLine.push(point1);
             tmpAttLine.push(point2);
             newPolyline.addPath(tmpAttLine);
